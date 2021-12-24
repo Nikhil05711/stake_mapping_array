fcac46f52e5650189d2e0b344a3ce39879bd32f0
Nikhil05711 <Nikhil@123>
2021-12-24 11:06:25 +0530

    update the code acc. to new version

--- contracts/Procedural.sol
+++ contracts/Procedural.sol
@@ -1,41 +1,55 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.11;
 
 contract Courses {
-    
     struct Instructor {
-        uint age;
+        uint256 age;
         string fName;
         string lName;
     }
-    
-    mapping (address => Instructor) instructors;
+
+    mapping(address => Instructor) instructors;
     address[] public instructorAccts;
 
-	
-    
-    function setInstructor(address _address, uint _age, string memory _fName, string memory _lName) public {
+    function setInstructor(
+        address _address,
+        uint256 _age,
+        string memory _fName,
+        string memory _lName
+    ) public returns (uint256) {
         //var instructor = instructors[_address];
-        
+
         instructors[_address].age = _age;
         instructors[_address].fName = _fName;
         instructors[_address].lName = _lName;
-        
-        instructorAccts.push(_address) -1;
+
+        //  instructorAccts.push(_address) -1;
+        instructorAccts.push(_address);
+        uint256 value = instructorAccts.length - 1;
+        return value;
     }
 
-  
-    
-    function getInstructors() view public returns(address[] memory) {
+    function getInstructors() public view returns (address[] memory) {
         return instructorAccts;
     }
-    
-    function getInstructor(address _address) view public returns (uint, string memory, string memory) {
-        return (instructors[_address].age, instructors[_address].fName, instructors[_address].lName);
+
+    function getInstructor(address _address)
+        public
+        view
+        returns (
+            uint256,
+            string memory,
+            string memory
+        )
+    {
+        return (
+            instructors[_address].age,
+            instructors[_address].fName,
+            instructors[_address].lName
+        );
     }
-    
-    function countInstructors() view public returns (uint) {
+
+    function countInstructors() public view returns (uint256) {
         return instructorAccts.length;
     }
-    
 }
